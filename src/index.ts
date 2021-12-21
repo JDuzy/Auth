b1ef149ec6d73c7dc45617eff27876a22abd9b52
Juan Duzac <juan.duzac@naranjax.com>
2021-12-21 14:52:32 -0300

    Routes handlers

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import { json } from 'body-parser';
 
-const app = express();
-app.use(json())
+import { currentUserRouter } from './routes/currentuser';
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('hi there')
-});
+const app = express();
+app.use(json());
+app.use(currentUserRouter);
 
 app.listen(3000, () => {
     console.log("Listening on port 3000!!!")
