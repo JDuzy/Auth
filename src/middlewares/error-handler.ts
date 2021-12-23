0903d95ad1b503530c2aa11d52bc07a786249ebd
Juan Duzac <juan.duzac@naranjax.com>
2021-12-23 13:56:13 -0300

    Added error handler

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+import { CustomError } from "../errors/custom-error";
+
+export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
+
+    if (error instanceof CustomError){
+        return res.status(error.statusCode).send({
+            error: error.serializeErrors()
+        });
+    }
+
+    res.status(400).send({
+        errors: [{message: 'Unknown error'}]
+    })
+};
